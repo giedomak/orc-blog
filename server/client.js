b893447b8b1b569cd27119afe53597dcc4077de4
Maximiline Thayer <garbados@gmail.com>
2014-08-12 02:00:34 -0700

    script updates

--- server/client.js
+++ server/client.js
@@ -2,13 +2,14 @@ var Router = require('express').Router;
 
 var client = Router();
 
-client.get('/', function (req, res) { });
 client.get('/views/:name', function (req, res) {
   res.render(req.param.name, {}, function (err, html) {
     if (err) throw err;
     res.send(html);
   });
 });
-client.get('/*', function (req, res) { });
+
+client.get('/', function (req, res) { res.render('index'); });
+client.get('/*', function (req, res) { res.render('index'); });
 
 module.exports = client;
