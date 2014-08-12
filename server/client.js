fc3eb34f8cedc2dec739e4fa8ea94d7909b1b372
Maximiline Thayer <garbados@gmail.com>
2014-08-12 01:54:03 -0700

    client route and template updates

--- server/client.js
+++ server/client.js
@@ -3,6 +3,12 @@ var Router = require('express').Router;
 var client = Router();
 
 app.get('/', function (req, res) { });
+app.get('/views/:name', function (req, res) {
+  res.render(req.param.name, {}, function (err, html) {
+    if (err) throw err;
+    res.send(html);
+  });
+});
 app.get('/*', function (req, res) { });
 
 module.exports = client;
