c02ad6365b6e056ecc950b991da5ad4524bafb07
Maximiline Thayer <garbados@gmail.com>
2014-08-12 01:54:43 -0700

    typo

--- server/client.js
+++ server/client.js
@@ -2,13 +2,13 @@ var Router = require('express').Router;
 
 var client = Router();
 
-app.get('/', function (req, res) { });
-app.get('/views/:name', function (req, res) {
+client.get('/', function (req, res) { });
+client.get('/views/:name', function (req, res) {
   res.render(req.param.name, {}, function (err, html) {
     if (err) throw err;
     res.send(html);
   });
 });
-app.get('/*', function (req, res) { });
+client.get('/*', function (req, res) { });
 
 module.exports = client;
